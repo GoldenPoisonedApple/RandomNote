a390a587fe7dd788fd12009319967288684591b0
ito <nanisiyou1@gmail.com>
2024-03-05 23:51:48 +0900

    start Tag over again

---
+++ RandomNote/Assets/Script/File/I_TagControl.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+
+/// <summary>
+/// タグ操作 どうせTagDataWrapperの具現クラス使うからいらん気がするけど
+/// </summary>
+public interface I_TagControl {
+	/// <summary>
+	/// タグデータ取得
+	/// </summary>
+	/// <returns>全てのタグデータ</returns>
+	public List<TagData> GetDatas();
+	
+	/// <summary>
+	/// タグの名前更新
+	/// </summary>
+	/// <param name="num">更新先のタグ番号</param>
+	/// <param name="name">更新後の名前</param>
+	public void UpdateName(int num, string name);
+
+	/// <summary>
+	/// タグの使われてる数をインクリメント
+	/// <param name="num">インクリメントするタグ番号</param>
+	/// </summary>
+	public void Increment(int num);
+
+	/// <summary>
+	/// 使われてる数を更新
+	/// </summary>
+	/// <param name="num">更新先のタグ番号</param>
+	/// <param name="amount">更新後の使われてる数</param>
+	public void UpdateAmount(int num, int amount);
+
+	/// <summary>
+	/// タグを追加
+	/// </summary>
+	/// <param name="name">タグの名前</param>
+	/// <returns>追加したタグの保存番号</returns>
+	public int AddTag(string name);
+
+	/// <summary>
+	/// タグ削除
+	/// </summary>
+	/// <param name="num">削除するタグ番号</param>
+	public void DelTag(int num);
+
+	/// <summary>
+	/// タグの名前取得
+	/// </summary>
+	/// <param name="num">取得するタグ番号</param>
+	/// <returns>タグの名前</returns>
+	public string GetName(int num);
+
+	/// <summary>
+	/// タグの使われてる数取得
+	/// </summary>
+	/// <param name="num">取得するタグ番号</param>
+	/// <returns>タグの使われてる数</returns>
+	public int GetAmount(int num);
+}
--- RandomNote/Assets/Script/File/TagData.cs
+++ RandomNote/Assets/Script/File/TagData.cs
@@ -6,68 +6,26 @@ using System;
 [Serializable]
 public class TagData {
 
-    /// <summary>
-    /// タグ新規場所に作成時コンストラクタ
-    /// endかfirstにしか使わない
-    /// </summary>
-    /// <param name="word">タグ名</param>
-    /// <param name="num">タグ保存番号</param>
-    /// <param name="used_count">使用されている数</param>
-    /// <param name="pre">前のタグ保存番号</param>
-    /// <param name="next">次のタグ保存番号</param>
-    /// <param name="next">タグの種類</param>
-    public TagData (string word, int num, int used_count, int pre, int next, int state) {
-        this.word = word;
-        this.num = num;
-        this.used_count = used_count;
-        this.pre = pre;
-        this.next = next;
-        this.state = state;
-    }
-    /// <summary>
-    /// タグデータ追加
-    /// </summary>
-    /// <param name="word">タグ名</param>
-    /// <param name="num">タグ保存番号</param>
-    /// <param name="used_count">使用されている数</param>
-    public void addData(string word, int num, int used_count) {
-        this.word = word;
-        this.num = num;
-        this.used_count = used_count;
-        this.state = DATA;
-    }
+	public TagData (string name) {
+		this.name = name;
+		this.amount = 0;
+		this.status = DATA;
+	}
 
-    /// <summary>
-    /// タグ名
-    /// </summary>
-    public string word;
+	/// <summary>
+	/// タグ名
+	/// </summary>
+	public string name;
 
-    /// <summary>
-    /// タグ保存番号
-    /// </summary>
-    public int num;
+	/// <summary>
+	/// 使用されている数
+	/// </summary>
+	public int amount;
 
-    /// <summary>
-    /// 使用されている数
-    /// </summary>
-    public int used_count;
-
-    /// <summary>
-    /// 前のタグ保存番号
-    /// </summary>
-    public int pre;
-
-    /// <summary>
-    /// 次のタグ保存番号
-    /// </summary>
-    public int next;
-
-    /// <summary>
-    /// タグの種類, FIRSTかENDかDATA
-    /// </summary>
-    public int state;
-    public const int FIRST = 0;
-    public const int DATA = 1;
-    public const int END = 2;
-    public const int DELETE = 3;
+	/// <summary>
+	/// タグの種類, DATAかDELか
+	/// </summary>
+	public int status;
+	public const int DATA = 1;
+	public const int DEL = 3;
 }
