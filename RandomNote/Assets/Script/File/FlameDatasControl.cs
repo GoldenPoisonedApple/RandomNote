304f78f3e14fd58a4ea1e0776deb1f1ff84abdba
ito <nanisiyou1@gmail.com>
2024-03-11 22:12:54 +0900

    #31 I'm scared so I'll commit soon.

--- RandomNote/Assets/Script/File/FlameDatasControl.cs
+++ RandomNote/Assets/Script/File/FlameDatasControl.cs
@@ -5,33 +5,36 @@ using Codice.CM.Client.Differences;
 /// <summary>
 /// 単語データ
 /// </summary>
-public class FlameDatasControl : I_FlameDatas {
-
-	//検索の簡単化のため
-	private int next = 0;
-	private List<I_FlameData> flameDatas;
-
+public class FlameDatasControl <T>
+	//制約(I_FileContentというインターフェースを持っている型のみ)
+	where T : I_FlameData
+	{
 	/// <summary>
 	/// コンストラクタ
 	/// </summary>
 	/// <param name="flameDatas">参照渡し</param>
-	public FlameDatasControl (ref List<I_FlameData> flameDatas) {
+	public FlameDatasControl(ref List<T> flameDatas)
+	{
 		this.flameDatas = flameDatas;
 	}
+
+	//検索の簡単化のため
+	private int next = 0;
+	private List<T> flameDatas;
 
 	/// <summary>
 	/// フレームデータ全取得
 	/// </summary>
 	/// <returns>フレームデータ</returns>
-	public List<I_FlameData> GetDatas () {
+	public List<T> GetDatas () {
 		// 参照を渡さないようにコピーを作成し渡す
-		return new List<I_FlameData>(flameDatas);
+		return new List<T>(flameDatas);
 	}
 	/// <summary>
 	/// 有効フレームデータ全取得
 	/// </summary>
 	/// <returns>有効フレームデータ</returns>
-	public List<I_FlameData> GetValidDatas () {
+	public List<T> GetValidDatas () {
 		return flameDatas.FindAll(flame => flame.GetStatus()==I_FlameData.DATA);
 	}
 
@@ -41,7 +44,7 @@ public class FlameDatasControl : I_FlameDatas {
 	/// <returns>有効データ数</returns>
 	public int GetValidCount () {
 		int count = 0;
-		foreach (I_FlameData flameData in flameDatas) {
+		foreach (T flameData in flameDatas) {
 			if (flameData.GetStatus() == I_FlameData.DATA)
 				count++;
 		}
@@ -53,7 +56,7 @@ public class FlameDatasControl : I_FlameDatas {
 	/// </summary>
 	/// <param name="flameData">データ</param>
 	/// <returns>追加したindex番号</returns>
-	public int Add (I_FlameData flameData) {
+	public int Add (T flameData) {
 		next = SearchNext(next);
 		// 登録番号セット
 		flameData.SetNum(next);
@@ -75,11 +78,11 @@ public class FlameDatasControl : I_FlameDatas {
 	public void Del (int num) {
 		try
 		{
-			I_FlameData I_FlameData = flameDatas[num];
-			if (I_FlameData.GetStatus() == I_FlameData.DEL)
+			T T = flameDatas[num];
+			if (T.GetStatus() == T.DEL)
 				throw new Exception("The Data is already deleted");
 			else
-				I_FlameData.SetStatus(I_FlameData.DEL);
+				T.SetStatus(T.DEL);
 				
 			if (num < next)
 				next = num;
@@ -92,7 +95,7 @@ public class FlameDatasControl : I_FlameDatas {
 	/// </summary>
 	/// <param name="num">更新ワードデータ番号</param>
 	/// <param name="flameData">ワードデータ</param>
-	public void Update (int num, I_FlameData flameData) {
+	public void Update (int num, T flameData) {
 		try
 		{
 			if (flameDatas[num].GetStatus() == I_FlameData.DEL)
@@ -104,8 +107,7 @@ public class FlameDatasControl : I_FlameDatas {
 		}
 		catch (Exception) { throw; }
 	}
-
-
+	
 	private int SearchNext (int beginIndex) {
 		int index;
 		for (index=beginIndex; index<flameDatas.Count; index++) {
